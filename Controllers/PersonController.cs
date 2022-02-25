57b719af9a1bf4d0dd141999755800dd3a7e0864
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-02-25 16:40:09 +0100

    Three more attributes added to person

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -11,9 +11,9 @@ public class PersonController : ControllerBase
     [HttpGet]
     public IEnumerable<Person> Get()
     {
-        var person1 = new Person(1, "Terje Topland");
+        var person1 = new Person(1, "Terje Topland", "12.12.12", "12345678","@terje");
         
-        var person2 = new Person(2, "Bjarte Skaar");
+        var person2 = new Person(2, "Bjarte Skaar", "13.12.12", "876543231", "@bjarte");
 
         var persons = new List<Person>();
         
--- Models/Person.cs
+++ Models/Person.cs
@@ -6,10 +6,19 @@ public class Person
 
     public string? Name { get; set; } 
     
-    public Person(int id, string name)
+    public string Birth { get; set; }
+
+    public string Phone { get; set; }
+    
+    public string Email { get; set; }
+    
+    public Person(int id, string name, string birth, string phone, string email)
     {
         this.Id = id;
         this.Name = name;
+        this.Birth = birth;
+        this.Phone = phone;
+        this.Email = email;
     }
     
     public List<Coach>? Coaches { get; set; }
